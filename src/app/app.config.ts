0809f2bdbe5aeb2af6e3a67ff4e3e72babd00cf4
hxj0x <2063818987@qq.com>
2025-06-25 21:26:58 +0800

    using pnpm、update angular version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
-import { provideAnimations } from "@angular/platform-browser/animations";
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {provideAnimations} from "@angular/platform-browser/animations";
+import {ApplicationConfig, provideZonelessChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {provideEventPlugins} from '@taiga-ui/event-plugins';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), NG_EVENT_PLUGINS]
+  providers: [
+    provideAnimations(),
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideEventPlugins()
+  ]
 };
